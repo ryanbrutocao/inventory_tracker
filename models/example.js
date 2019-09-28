ce120838e8e1a5d5b3536db672e2fec780f7e04a
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-09-28 12:22:16 -0600

    initial commit with basic index.html and seed data

--- models/example.js
+++ models/example.js
@@ -1,7 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var main_inventory = sequelize.define("main_inventory", {
+
+    customer_name: DataTypes.TEXT,
+    varietal: DataTypes.STRING,
+    actual_ordered: DataTypes.INTEGER,
+    promised: DataTypes.INTEGER,
+    box_1_actual: DataTypes.INTEGER,
+    box_2_actual: DataTypes.INTEGER,
+    box_3_actual: DataTypes.INTEGER,
+    box_1_promised: DataTypes.INTEGER,
+    box_2_promised: DataTypes.INTEGER,
+    box_3_promised: DataTypes.INTEGER,
+    label_count: DataTypes.INTEGER,
+    date_ordered: DataTypes.DATE,
+    notes: DataTypes.TEXT
   });
-  return Example;
+  return main_inventory;
 };
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,23 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS main_wine;
+CREATE DATABASE main_wine;
+USE main_wine;
+CREATE TABLE main_inventory
+(
+	id int NOT NULL AUTO_INCREMENT,
+customer_name varchar(255) NOT NULL,
+varietal varchar(100) NOT NULL,
+actual_ordered varchar(255) NOT NULL,
+promised varchar(100) NOT NULL,
+box_1_promised integer(10) NOT NULL,
+box_2_promised integer(10) NOT NULL,
+box_3_promised integer(10) NOT NULL,
+box_1_actual integer(10) NOT NULL,
+box_2_actual integer(10) NOT NULL,
+box_3_actual integer(10) NOT NULL,
+label_count integer(10) NOT NULL,
+date_ordered datetime NOT NULL,
+notes varchar(1000) NOT NULL,
+	PRIMARY KEY (id)
+);
+INSERT INTO main_inventory (customer_name, varietal, actual_ordered, promised, box_1_promised, box_2_promised, box_3_promised, box_1_actual, box_2_actual, box_3_actual, label_count, date_ordered, notes) VALUES ('Melissa', 'Pinot Noir', '100', '300', '30', '30', '30', '20', '20', '20', '400', '01/10/19', 'these are my notes' );
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
