a5e5f49480ecb3c9385e9a589b48b8a32302b207
Brian Carver <brarver@yahoo.com>
2019-09-28 21:30:46 -0600

    working on maininventory api route

--- models/mainInventory.js
+++ models/mainInventory.js
@@ -2,12 +2,16 @@ var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
   var mainInventory = sequelize.define("mainInventory", {
-    wine: DataTypes.TEXT,
+    wine: DataTypes.STRING,
     actualInventory: DataTypes.INTEGER,
     shadowInventory: DataTypes.INTEGER,
     boxType: DataTypes.INTEGER,
     createdAt: {
       field: "createdAt",
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      field: "updatedAt",
       type: Sequelize.DATE
     }
   });
