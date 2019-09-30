2b59a7ea92fd88bb8920a3adda07ad256230eadc
brarver <brarver@yahoo.com>
2019-09-30 12:57:36 -0600

    get put delete post api routes finished

--- models/customerInfo.js
+++ models/customerInfo.js
@@ -4,11 +4,19 @@ module.exports = function(sequelize, DataTypes) {
   var customerInfo = sequelize.define(
     "customerInfo",
     {
-      clientName: DataTypes.TEXT,
+      clientName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: "none"
+      },
       primaryContact: DataTypes.TEXT,
       phone: DataTypes.TEXT,
       email: DataTypes.TEXT,
       streetAddress: DataTypes.TEXT,
+      city: {
+        type: DataTypes.TEXT,
+        defaultValue: "none"
+      },
       ST: DataTypes.TEXT,
       zipcode: DataTypes.TEXT,
       createdAt: {
