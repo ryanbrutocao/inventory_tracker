2a2261baeaeec1bb8ca423fc33facceaba82e753
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-10-03 14:47:43 -0600

    split labels table and fixed seeds/schema

--- models/seeds.sql
+++ models/seeds.sql
@@ -19,23 +19,23 @@ VALUES
 
 INSERT INTO customerInfo (clientName, primaryContact, phone, email, streetAddress, city, ST, zipcode) 
 VALUES 
-	("Melissa", "Melissa", "123-456-7890","melissa@winecompany.com", "123 Main St", "New York City", "NY", "01234"),
+	("Mack", "Mack", "123-456-7890","melissa@winecompany.com", "123 Main St", "New York City", "NY", "01234"),
 	("Jared", "Jared", "234-456-7890","jared@winecompany.com", "123 Main St", "San Francisco", "CA", "23456"),
 	("Steven", "Steven", "345-456-7890", "steven@winecompany.com", "123 Main St", "Charlotte", "NC", "34567");
 
 
 INSERT INTO orders (accountName, vintage, varietal, actualOrdered, promised, boxType, notes)
 VALUES
-	("Jones", "2016", "Chardonnay", 0, 100, "0",  "test"),
-	("Mack", "2017", "Pinot Noir", 0, 200, "0",  "test"),
-	("Smith", "2018", "Sauvignon Blanc", 0, 0,  "0", "test"),
-	("Mack", "2017", "Pinot Noir", 100, 0, "0", "test");
+	("Jones", "2016", "Chardonnay", 0, 100, "WRT",  "test"),
+	("Mack", "2017", "Pinot Noir", 0, 200, "WRU",  "test"),
+	("Smith", "2018", "Sauvignon Blanc", 0, 0,  "WBAJA", "test"),
+	("Mack", "2017", "Pinot Noir", 100, 0, "WRU", "test");
 
-INSERT INTO labels (accountName, wine, labelsLeft)
+INSERT INTO labels (accountName, vintage, varietal, promised, labelsLeft)
 VALUES
-	("Mack", "2017 Pinot Noir", 300),
-    ("Jones", "2017 Pinot Noir", 400),
-    ("Smith", "2017 Pinot Noir", 500);
+	("Mack", "2017", "Pinot Noir", 1000, 300),
+    ("Jared", "2017", "Chardonnay", 1000, 400),
+    ("Steven", "2017", "Merlot", 1000, 500);
 	
 
 SELECT * FROM customerInfo;
